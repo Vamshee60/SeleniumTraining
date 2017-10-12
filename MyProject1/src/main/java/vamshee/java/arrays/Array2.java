c1032f4fbfa45263301d18269b627cb69c1c14f6
Vamshee60 <vamsheekrishna60@gmail.com>
2017-10-13 10:28:33 +1100

    Revert "Revert "Assignments""
    This reverts commit 136277d57f6ca05d2647925b35ce3c481afe0444.

---
+++ MyProject1/src/main/java/vamshee/java/arrays/Array2.java
@@ -0,0 +1,12 @@
+package vamshee.java.arrays;
+
+public class Array2 {
+	
+	public static void main(String[] args)
+	{
+		for (int i = 1; i <= 10; i++)
+			System.out.println(i);
+			}
+	
+
+}
---
+++ MyProject1/src/main/java/vamshee/java/arrays/StringArray.java
@@ -0,0 +1,16 @@
+package vamshee.java.arrays;
+
+import java.util.Arrays;
+
+public class StringArray {
+
+	public static void main(String[] args) {
+		
+		String[] thisIsAStringArray = { "Vamshee Krishna" };
+			String theString = Arrays.toString(thisIsAStringArray);
+		System.out.println( theString );
+		}
+
+	}
+
+
