eb63d153ee354c309ca199b1f2421b4173f96f4a
mvdspk <123mvds@gmail.com>
2017-12-12 20:11:26 -0800

    temp

---
+++ MyProject1/src/main/java/date/DateDemo.java
@@ -0,0 +1,26 @@
+package date;
+
+import java.util.Date;
+
+public class DateDemo {
+
+	
+	
+	public static void main(String[] args) {
+		Date date= new Date();
+
+		
+		// print today's date
+		System.out.println("Todays Date : "+date.toString());
+		
+		// Date Format
+		
+		// Time Zone
+		
+		
+		
+	}
+	
+	
+	
+}
---
+++ MyProject1/src/main/java/vamshee/java/primitivetypes/MaxValue.java
@@ -0,0 +1,11 @@
+package vamshee.java.primitivetypes;
+
+public class MaxValue {
+	
+	public static void main(String[] args) {
+		int a[]={1,4,5,3,66,7,55,88};
+		
+		int noOfThreads=
+	}
+
+}
